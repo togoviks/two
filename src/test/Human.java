78bf1311a5dcfccf47c396ac1c43766ce29a0d22
osipov <togoviks@gmail.com>
2019-12-02 16:29:12 +0300

    first

---
+++ src/test/Human.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Human {
+    String name;
+    String secondName;
+
+        public Human(String name, String secondName) {
+            this.name = name;
+            this.secondName = secondName;
+        }
+}
--- src/test/Main.java
+++ src/test/Main.java
@@ -2,6 +2,6 @@ package test;
 
 public class Main {
     public static void main(String[] args) {
-        new App().start();
+        new Array().printArray();
     }
 }
